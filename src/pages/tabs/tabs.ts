4372326d759428224e6e6523948f84e325a9a727
Matt Harnett <harnettmatt@gmail.com>
2018-05-02 16:04:19 -0700

    Tempate added
    tabs for tasks and calendar are added in

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { CalendarPage } from '../calendar/calendar';
+import { TasksPage } from '../tasks/tasks';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = CalendarPage;
+  tab2Root = TasksPage;
 
   constructor() {
 
