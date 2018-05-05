15e8854bbad0f7e843c2b88d28486781a5e66356
Matt Harnett <harnettmatt@gmail.com>
2018-05-04 17:31:52 -0700

    basic form template

--- src/pages/new-task/new-task.ts
+++ src/pages/new-task/new-task.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the NewTaskPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,11 +9,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class NewTaskPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    newTask = {};
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad NewTaskPage');
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    }
+
+    sumbitForm() {
+        console.log(this.newTask);
+    }
 
 }
