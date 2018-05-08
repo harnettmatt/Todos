f6018ab12382e3e5bd6be091c9d4a7626acf292c
Matt Harnett <harnettmatt@gmail.com>
2018-05-07 17:43:58 -0700

    using form builder
    - allows me to add fields in later
    - dont need to initialize the object before using the form
    - because of this funcitionality we can use the native validation provided by the interface of manadtory fields and optional fields
    
    Can look into a cleaner way to do this later

--- src/pages/new-task/new-task.ts
+++ src/pages/new-task/new-task.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 
 interface Task {
-    completed?: boolean;
-    name?: string;
+    completed: boolean;
+    name: string;
     description?: string;
     due?: string;
     duration?: number;
@@ -19,16 +20,33 @@ interface Task {
 })
 export class NewTaskPage {
 
+    newTaskForm: FormGroup;
     newTask: Task;
     tasksCollection: AngularFirestoreCollection<Task>;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, private afs: AngularFirestore) {
-        this.newTask = {};
+    constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder, private afs: AngularFirestore) {
+        this.newTaskForm = this.formBuilder.group({
+            name: [''],
+            description: [''],
+            due: [''],
+            duration: [''],
+            durationUnit: [''],
+            priority: ['']
+        });
         this.tasksCollection = this.afs.collection('tasks');
     }
 
     submitForm() {
-        this.newTask.completed = false;
+        console.log(this.newTaskForm.value);
+        this.newTask = {
+            completed: false,
+            name: this.newTaskForm.value['name'],
+            due: this.newTaskForm.value['due'],
+            duration: this.newTaskForm.value['duration'],
+            durationUnit: this.newTaskForm.value['durationUnit'],
+            priority: this.newTaskForm.value['priority']
+        }
+        console.log(this.newTask);
         this.tasksCollection.add(this.newTask);
         this.navCtrl.pop();
     }
