e2c67cdaa09500ce9a0083a9b8078a889b31f5ee
Matt Harnett <harnettmatt@gmail.com>
2018-05-09 15:54:01 -0700

    simple calendar template

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -1,25 +1,48 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the CalendarPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+interface CalendarIncrement {
+    label: string;
+    time: Number;
+    border: string;
+}
 
 @IonicPage()
 @Component({
-  selector: 'page-calendar',
-  templateUrl: 'calendar.html',
+    selector: 'page-calendar',
+    templateUrl: 'calendar.html',
 })
 export class CalendarPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    calendar: CalendarIncrement[];
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CalendarPage');
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+        this.buildCalendar();
 
+    }
+
+    buildCalendar() {
+        this.calendar = [];
+        for (let x=0; x<=96; x++) {
+            let border = '0px solid';
+            let label = '';
+            let total_mins = x*15;
+            let mins = total_mins % 60;
+            let hours = Math.floor(total_mins / 60);
+            let time = (hours*100) + mins;
+            if (mins == 0) {
+                label = String(hours) + ":" + String(mins) + "0";
+            }
+            if (mins == 45) {
+                border = '1px solid';
+            }
+            let increment = {
+                'label': label,
+                'time': time,
+                'border': border
+            }
+            console.log(increment);
+            this.calendar.push(increment);
+        }
+    }
 }
