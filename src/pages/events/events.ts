c15d78475c07f2ad59643477f11675c6eff62125
Matt Harnett <harnettmatt@gmail.com>
2018-05-11 16:45:13 -0700

    logic and layout for the events page

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -3,6 +3,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 
+interface Event {
+    name: string;
+    from: string; //should this be a date
+    to: string; //should this be a date
+    frequency: string[];
+}
+
 @IonicPage()
 @Component({
     selector: 'page-events',
@@ -10,8 +17,26 @@ import { Observable } from 'rxjs/Observable';
 })
 export class EventsPage {
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, private afs: AngularFirestore) {
+    eventsCollection: AngularFirestoreCollection<Event>;
+    events: Observable<Event[]>;
 
+    constructor(public navCtrl: NavController, public navParams: NavParams, private afs: AngularFirestore) {
+        this.eventsCollection = this.afs.collection('events');
+        this.events = this.eventsCollection.snapshotChanges().map(actions => {
+            return actions.map(a => {
+                const data = a.payload.doc.data() as Event;
+                const id = a.payload.doc.id;
+                return { id, ...data };
+            });
+        });
     }
+
+    // addEvent() {
+    //     this.navCtrl.push(NewEventPage);
+    // }
+    //
+    // editTask(event) {
+    //     this.navCtrl.push(EditEventPage, { data: event });
+    // }
 
 }
