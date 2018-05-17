670b8d586617a18ad529e5d2b17b22ab397a0aae
Matt Harnett <harnettmatt@gmail.com>
2018-05-16 17:26:09 -0700

    added schedule coloring based on preferences

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable'
 
 interface CalendarIncrement {
     label: string;
     time: Number;
     border: string;
+    color: string;
+}
+
+interface Preference {
+    id: string;
+    name: string;
+    from: Number;
+    to:   Number;
 }
 
 @IonicPage()
@@ -16,8 +26,11 @@ export class CalendarPage {
 
     calendar: CalendarIncrement[];
     date: any;
+    preferencesCollection: AngularFirestoreCollection<Preference>;
+    preferencesSnapshot: Observable<Preference[]>;
+    preferences = [];
 
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    constructor(public navCtrl: NavController, public navParams: NavParams, private afs: AngularFirestore) {
         if (this.navParams.get('date')) {
             this.date = this.navParams.get('date');
         }
@@ -25,7 +38,9 @@ export class CalendarPage {
             this.date = new Date();
         }
         this.buildCalendar();
+        this.buildPreferences();
     }
+
 
     buildCalendar() {
         this.calendar = [];
@@ -56,10 +71,49 @@ export class CalendarPage {
             let increment = {
                 'label': label,
                 'time': time,
-                'border': border
+                'border': border,
+                'color': 'white'
             }
             this.calendar.push(increment);
         }
+    }
+
+    buildPreferences() {
+        this.preferencesCollection = this.afs.collection('preferences');
+        this.preferencesSnapshot = this.preferencesCollection.snapshotChanges().map(actions => {
+            return actions.map(a => {
+                let data = a.payload.doc.data() as Preference;
+                const id = a.payload.doc.id;
+                data.id = id
+                return { id, ...data };
+            });
+        });
+        this.preferencesSnapshot.forEach(array => {
+            for (let preference of array) {
+                if (preference.name == 'sleep'){
+                    for (let increment of this.calendar) {
+                        if (preference.to > increment.time && increment.time >= 0) {
+                            increment.color = 'gray';
+                        }
+                        if (2400 >= increment.time && increment.time >= preference.from){
+                            increment.color = 'gray';
+                        }
+                    }
+                }
+                else if (preference.name == 'work') {
+                    for (let increment of this.calendar) {
+                        if (preference.to > increment.time && increment.time >= preference.from) {
+                            increment.color = 'lightblue';
+                        }
+                    }
+                }
+                this.preferences.push(preference);
+            }
+        });
+
+
+        // determine the increments needed for each preference
+        // change color and borders of increment of calendar to reflect the event
     }
 
     previousDay() {
