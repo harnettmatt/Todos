7718ba2717f23accab814598c3bb83aab89b99d1
Matt Harnett <harnettmatt@gmail.com>
2018-11-17 20:15:00 -0800

    checkboxes work for recurring days
    they dont look good but they serve their purpose. Ionic has no native support to change out the checkbox icons. so this will do for now

--- src/pages/new-event/new-event.ts
+++ src/pages/new-event/new-event.ts
@@ -12,8 +12,10 @@ import { Event } from '../events/events';
 export class NewEventPage {
 
     newEventForm: FormGroup;
+    daysForm: FormGroup;
     newEvent: Event;
     eventsCollection: AngularFirestoreCollection<Event>;
+    days: any;
 
     constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder, private afs: AngularFirestore) {
         this.newEventForm = this.formBuilder.group({
@@ -21,21 +23,48 @@ export class NewEventPage {
             from: [''],
             to: [''],
             common: [''],
-            days: [''],
-
+            sunday: ['false'],
+            monday: ['false'],
+            tuesday: ['false'],
+            wednesday: ['false'],
+            thursday: ['false'],
+            friday: ['false'],
+            saturday: ['false']
         });
         this.eventsCollection = this.afs.collection('events');
     }
 
+    buildDays() {
+        let arr = this.days.map(day => {
+            return this.formBuilder.control(day.selected);
+        });
+        return this.formBuilder.array(arr);
+    }
+
     submitForm() {
+        this.days = {
+            sunday: this.newEventForm.value['sunday'],
+            monday: this.newEventForm.value['monday'],
+            tuesday: this.newEventForm.value['tuesday'],
+            wednesday: this.newEventForm.value['wednesday'],
+            thursday: this.newEventForm.value['thursday'],
+            friday: this.newEventForm.value['friday'],
+            saturday: this.newEventForm.value['saturday']
+        }
+
         this.newEvent = {
             name: this.newEventForm.value['name'],
-            from: this.newEventForm.value['name'],
-            to: this.newEventForm.value['name'],
+            from: this.newEventForm.value['from'],
+            to: this.newEventForm.value['to'],
             common: false,
-            days: this.newEventForm.value['name'],
+            days: this.days
         }
+
         this.eventsCollection.add(this.newEvent);
         this.navCtrl.pop();
+    }
+
+    updateDays() {
+        console.log(this.days);
     }
 }
